4322e567ad92eefd63fd2b810b4e855eabaf7088
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2017-01-11 00:04:34 -0800

    can add muliple loans

--- src/js/scripts/general.js
+++ src/js/scripts/general.js
@@ -1,24 +1,48 @@
 function cost() {
-	$('.final-cost').val(
-		$('[name="car-cost"]').val().replace(/\,/g, '').replace(/\$/g, '').replace(/\ /g, '') -
-		$('[name="downPayment"]').val().replace(/\,/g, '').replace(/\$/g, '').replace(/\ /g, '') -
-		$('[name="tradeIn"]').val().replace(/\,/g, '').replace(/\$/g, '').replace(/\ /g, '')
-	);
+	$('.estimate-payments').each(function(){
+		$(this).find('.final-cost').val(
+			$(this).find('.car-cost').val().replace(/\,/g, '').replace(/\$/g, '').replace(/\ /g, '') -
+			$(this).find('.downPayment').val().replace(/\,/g, '').replace(/\$/g, '').replace(/\ /g, '') -
+			$(this).find('.tradeIn').val().replace(/\,/g, '').replace(/\$/g, '').replace(/\ /g, '')
+		);
 
-	$('[type="range"]').each(function(){
-		var value = $(this).val();
-		// if( $(this).hasClass('three-decimal') ) {
-		// 	value = parseFloat(value).toFixed(3);
-		// }
-		$(this).next().find('input').val(value);
+		if( $(this).find('.final-cost').val() <= 0) {
+			$(this).find('.final-cost').val(0);
+		}
 	});
+}
 
-	if( $('.final-cost').val() <= 0) {
-		$('.final-cost').val(0);
-	}
+function calcResults() {
+	var loan = 0;
+	$('.estimate-payments').each(function(){
+		$(this)
+		.accrue({
+			response_output_div: $('.loan-overview').eq(loan).find('.monthly-payment .results'),
+			response_basic:
+				'$%payment_amount%',
+			error_text: "\u2014\u2014\u2014",
+		})
+		.accrue({
+			response_output_div: $('.loan-overview').eq(loan).find('.total-interest .results'),
+			response_basic:
+				'$%total_interest%',
+			error_text: "\u2014\u2014\u2014",
+		})
+		.accrue({
+			response_output_div: $('.loan-overview').eq(loan).find('.total-cost .results'),
+			response_basic:
+				'$%total_payments%',
+			error_text: "\u2014\u2014\u2014",
+		});
+
+		loan = loan + 1;
+	});
 }
 
 $(function(){
+	cost();
+	calcResults();
+
 	$('.results.button').on('click', function(){
 		$('.payment-overview').toggleClass('view-results');
 		$(this).find('i').toggleClass('fa-rotate-180');
@@ -30,36 +54,47 @@ $(function(){
 		}
 	});
 
-	cost();
-	$('.estimate-payments input').on('keyup click', function(){
+	$('body').on('keyup click', '.estimate-payments input', function(){
 		cost();
+		calcResults();
 	});
 
-	// $('.rate').priceFormat({
-	// 	prefix: '',
-	// 	suffix: '%'
-	// });
 	$('.rate').mask('99.999%',{
 		placeholder:"_"
 	});
 
-	$(".estimate-payments")
-	.accrue({
-		response_output_div: ".monthly-payment .results",
-		response_basic:
-			'$%payment_amount%',
-		error_text: "\u2014\u2014\u2014",
-	})
-	.accrue({
-		response_output_div: ".total-interest .results",
-		response_basic:
-			'$%total_interest%',
-		error_text: "\u2014\u2014\u2014",
-	})
-	.accrue({
-		response_output_div: ".total-cost .results",
-		response_basic:
-			'$%total_payments%',
-		error_text: "\u2014\u2014\u2014",
+	$('.loans-list').on('click', 'li', function(){
+		$('.loans-list li').removeClass('active');
+
+		if( $(this).hasClass('add-loan') ) {
+			$('.estimate-payments').eq(0).clone().insertAfter( $('.estimate-payments').last() );
+			$('.estimate-payments').removeClass('active').last().addClass('active');
+			$('<li>Loan ' +  ($('.loans-list li').length) + '</li>').insertBefore( $('.loans-list li').last() );
+			$('.loans-list li').last().prev().addClass('active');
+			$('.loan-overview').eq(0).clone().insertAfter( $('.loan-overview').last() );
+
+			cost();
+			calcResults();
+
+		} else {
+			$(this).addClass('active');
+			var activeLoan = 0;
+			$('.loans-list li').each(function(){
+				if( !$(this).hasClass('active') ) {
+					activeLoan = activeLoan + 1;
+				} else {
+					$('.estimate-payments').removeClass('active').eq(activeLoan).addClass('active');
+
+					return true;
+				}
+			});
+		}
 	});
+
+
+});
+
+$(function() {
+  $(document).on("touchmove", function(evt) { evt.preventDefault() });
+  $(document).on("touchmove", ".scrollable", function(evt) { evt.stopPropagation() });
 });
